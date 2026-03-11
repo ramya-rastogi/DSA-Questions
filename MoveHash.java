e7d4c4e86155e9362b210e802a89e528a2d2a6d0
Ramya Rastogi <ramyarastogi25@gmail.com>
2026-03-11 10:28:26 +0530

    Add files via upload

---
+++ MoveHash.java
@@ -0,0 +1,7 @@
+public class MoveHash {
+    public static void main(String[] args) {
+        String s = "Move#Hash#to#Front";
+        String result = s.replace("#", "");
+        System.out.println(result);
+    }
+}
